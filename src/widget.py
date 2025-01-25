3f4f799026d6f1a9d3004a8877329088773fdee8
Учеба <you@example.com>
2025-01-25 13:38:27 +0300

    Recoding function mask_account_card, use variables

--- src/widget.py
+++ src/widget.py
@@ -9,10 +9,12 @@ from src.masks import get_mask_account, get_mask_card_number
 def mask_account_card(type_card_or_account: str) -> str:
     """Обрабатывает информацию как о картах, так и о счетах. Преобразует в маскированный вид"""
     splited_items = type_card_or_account.split()
+    type_of_card_account = ' '.join(splited_items[:-1])
     if splited_items[0] == "Счет":
-        return ' '.join(splited_items[:-1]) + ' ' + get_mask_account(splited_items[-1])
+        number_of_card_account = get_mask_account(splited_items[-1])
     else:
-        return ' '.join(splited_items[:-1]) + ' ' + get_mask_card_number(splited_items[-1])
+        number_of_card_account = get_mask_card_number(splited_items[-1])
+    return type_of_card_account + ' ' + number_of_card_account
 
 
 def get_date(unformated_date: str) -> str:
