622517acb99bced8e3b8537e8840a2f971eca9e3
Babushkin Andrey <babushkinflyer@gmail.com>
2025-01-30 00:31:01 +0300

    Fix by linters

--- src/widget.py
+++ src/widget.py
@@ -1,20 +1,15 @@
-"""
-Функция обрабатывает информацию как о картах, так и о счетах
-и возвращает строку с замаскированным номером.
-"""
-
 from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(type_card_or_account: str) -> str:
     """Обрабатывает информацию как о картах, так и о счетах. Преобразует в маскированный вид"""
     splited_items = type_card_or_account.split()
-    type_of_card_account = ' '.join(splited_items[:-1])
+    type_of_card_account = " ".join(splited_items[:-1])
     if splited_items[0] == "Счет":
         number_of_card_account = get_mask_account(splited_items[-1])
     else:
         number_of_card_account = get_mask_card_number(splited_items[-1])
-    return type_of_card_account + ' ' + number_of_card_account
+    return type_of_card_account + " " + number_of_card_account
 
 
 def get_date(unformated_date: str) -> str:
